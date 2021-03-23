89c2564e0c01563c8065e30674a09c01ca33fda6
VAD <vladimir_ad@mail.ru>
2021-03-24 00:11:03 +0300

    Добавлено решение задачи Z_C Изменения, которые будут включены в коммит: 	изменено:      array/ArrayC/Z_C/main.cpp

--- array/ArrayC/Z_C/main.cpp
+++ array/ArrayC/Z_C/main.cpp
@@ -2,26 +2,58 @@
 
 using namespace std;
 
+/** \details Массив gt служи для хранения обработанных чисел, что
+позволяет не повторяться в подсчете одинаковых чисел. Количество
+записанных в массив чисел хранится в переменной gt_size
+*/
+static int gt[101];
+static int gt_size;
+
+/** \details Если зачение value уже находится в массисе gt, то
+возвращается -1, иначе это значение заносится в массив, изменяется
+количество чисел - gt_size.
+*/
+int gt_add(int value)
+{
+    for(int j = 0; j < gt_size; j++)
+    {
+        if(gt[j] == value)
+            return -1;
+    }
+
+    gt[gt_size] = value;
+    gt_size++;
+    return 0;
+}
+
 int main()
 {
-    int j, n, a[101];
+    int n;
+    int a[101];
     cin >> n;
     for(int i = 0; i < n; i++)
         cin >> a[i];
-
-    int all, res = 0;
+    gt_size = 0;
+    int res = 0;
     for(int i = 0; i < n; i++)
     {
-        j = a[i];
-        for(int k = 0; k < n; i++)
+        int j = a[i];
+        if(gt_add(j) == 0)
         {
+            /* здесь если число попалось впервые */
+            /* считываем количество одинаковых чисел...*/
+            int cnt = 0;
+            for(int k = 0; k < n; k++)
             {
-                if(j == a[k])
-                    res++;
+                {
+                    if(j == a[k])
+                        cnt++;
+                }
             }
-            res--;
-            all += res;
+            /* определяем количество пар.*/
+            res += cnt/2;
         }
     }
+    cout << res << endl;
     return 0;
 }
