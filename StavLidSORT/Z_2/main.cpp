156fadb26ccc3d5b5e1d88bf1dc01c399deae2e6
Lemon <raccoonreckless@gmail.com>
2021-04-01 23:13:35 +0300

    res

--- StavLidSORT/Z_2/main.cpp
+++ StavLidSORT/Z_2/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a[1002], k, m, n, h = 0;
+    cin >> k >> m >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for(int i = 1; i < n; i++)
+        for(int j = n-1; j >= i; j--)
+            if(a[j] < a[j-1])
+                swap(a[j], a[j-1]);
+
+    for(int i = 0; i < k; i++)
+        m -= a[i];
+
+    cout << m << endl;
     return 0;
 }
