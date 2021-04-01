2df90817e817480306c54a289f76e5e6bbeba8ba
Lemon <raccoonreckless@gmail.com>
2021-04-01 22:27:16 +0300

    Rrsh

--- StavLidSORT/Z_1/main.cpp
+++ StavLidSORT/Z_1/main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int A[1002], n, res = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    for(int i = 1; i < n; i++)
+        for(int j = n-1; j >= i; j--)
+            if(A[j] < A[j-1])
+                swap(A[j], A[j-1]);
+
+    n = n/2+1;
+    for(int i = 0; i < n; i++)
+        res += A[i]/2+1;
+
+    cout << res << endl;
     return 0;
 }
