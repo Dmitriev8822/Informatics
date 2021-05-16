361deed6626d30c02aebfbf42342bda93c555cb9
Lemon <raccoonreckless@gmail.com>
2021-05-16 16:44:20 +0300

    T

---
+++ 27_SL/SF/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char line[20]
+    while(getline(cin,20))
+    {
+    cout << line << endl;
+    //do something
+    }
+    return 0;
+}
