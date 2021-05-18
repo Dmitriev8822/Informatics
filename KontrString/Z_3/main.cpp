13ed8c54128ed99d2ce798f9f22d8a3b67dd1a95
VAD <vladimir_ad@mail.ru>
2021-05-18 21:00:21 +0300

    Изменения, которые будут включены в коммит: новый файл:    KontrString/Z_3/Z_3.cbp   Добавлено решение третьей задачи. новый файл:    KontrString/Z_3/main.cpp

---
+++ KontrString/Z_3/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+
+char* skipDelimiters(char *p)
+{
+    while(*p == ' ')
+        p++;
+
+    return p;
+}
+
+char* getTaiOflWord(char *p)
+{
+    //while((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z'))
+    while(*p != ' ' && *p != 0)
+        p++;
+
+    return p;
+}
+
+void printWords(char *p)
+{
+    while(1)
+    {
+        p = skipDelimiters(p);
+        if(*p == 0)
+            break;
+        char *bgn = p;
+        p = getTaiOflWord(p);
+        if(*p == 0)
+        {
+            puts(bgn);
+            break;
+        }
+        *p = 0;
+        puts(bgn);
+        p++;
+    }
+}
+
+int main ()
+{
+    char str[502];
+    cin.getline(str, 501);
+    printWords(str);
+
+    return 0;
+}
