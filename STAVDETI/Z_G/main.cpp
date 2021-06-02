c1269f1c4ce4ff5f8db2b47a07b6ad7aa22f7aed
DmitrievVA <DmitrievVA@monocrystal.com>
2021-06-02 13:23:18 +0300

    Changes to be committed: 	modified:   STAVDETI/Z_G/main.cpp

--- STAVDETI/Z_G/main.cpp
+++ STAVDETI/Z_G/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-#include <fstream>
+//#include <fstream>
 using namespace std;
 
 int main()
 {
-    ifstream fin("input.txt");
-    ofstream fout("output.txt");
+    //ifstream fin("input.txt");
+    //ofstream fout("output.txt");
 
     int n;
-    fin >> n;
+    //fin >> n;
+    cin >> n;
     int maxF = 0;
     int maxV = 0;
 
     for(int i = 0; i < n; i++)
     {
         int t;
-        fin >> t;
+        //fin >> t;
+        cin >> t;
         if(t <= maxV)
             continue;
         if(t > maxF)
@@ -23,10 +25,14 @@ int main()
             maxV = maxF;
             maxF = t;
         }
+        else
+            if(t > maxV)
+                maxV = t;
     }
 
-    fout << maxV << endl;
-    fout.close();
-    fin.close();
+    //fout << maxV << endl;
+    cout << maxV << endl;
+    //fout.close();
+    //fin.close();
     return 0;
 }
