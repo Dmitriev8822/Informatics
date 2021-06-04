dbf56ed813c1178609e0c2d16986cdb05c3a62bb
VAD <vladimir_ad@mail.ru>
2021-06-04 23:37:26 +0300

    Изменения, которые будут включены в коммит: 	удалено:       27_SL/Z_6/main.cpp 	изменено:      STAVDETI/Z_C(Vlad)/main.cpp 	изменено:      STAVDETI/Z_C/main.cpp

--- STAVDETI/Z_C/main.cpp
+++ STAVDETI/Z_C/main.cpp
@@ -1,22 +1,77 @@
 #include <iostream>
-//#include <fstream>
-//    ofstream fout;
- //   fout.open("output.txt");
+
 using namespace std;
+
+//int*** new3DArray(int x, int y, int z)
+//{
+//    int ***res = new int**[x];
+//    for(int j=0; j < x; j++)
+//    {
+//        res[j] = new int*[y];
+//        for(int k=0; k < y; k++)
+//        {
+//            res[j][k] = new int[z];
+//        }
+//    }
+//    return res;
+//}
+//void delete3DArray(int*** p, int x, int y)
+//{
+//    for(int j=0; j < x; j++)
+//    {
+//        for(int k=0; k < y; k++)
+//        {
+//            delete[] p[j][k];
+//        }
+//        delete[] p[j];
+//    }
+//    delete[] p;
+//}
 
 int main()
 {
-    int i, j, z;
     int N, M, K, I;
     cin >> N >> M >> K >> I;
-    t = N*M*K;
-    int sp = t;
-    int a;
-    int arr = [10000];
-    for(int i = 0; i < t; i++)
-    {
-        cin >> a;
-        arr[sp] = a;
-        sp
-    return 0;
+    // создаем трехмерный массив
+    //int ***arr = new3DArray(N,M,K);
+
+    //вводим данные в массив
+    int i = N*M*K;
+    for(int x = 0; x < N; x++)
+        for(int y = 0; y < M; y++)
+            for(int z = 0; z < K; z++)
+            {
+                //cin >> arr[x][y][z];
+                //printf("i=%d, V=%d, %d:%d:%d\n", i--, arr[x][y][z], x,y,z);
+                int v;
+                cin >> v;
+                if(i-- == I)
+                {
+                    printf("%d %d %d %d\n", ++x, ++y, ++z, v);
+                    return 0;
+                }
+            }
+//    I--;
+//    i = 0;
+//    int x = N-1;
+//    for(; x>=0; x--)
+//    {
+//        int y = M-1;
+//        for(; y >= 0; y--)
+//        {
+//            int z = K-1;
+//            for(; z >= 0; z--)
+//            {
+//                if(i == I)
+//                {
+//                    printf("%d %d %d %d\n", x+1, y+1, z+1, arr[x][y][z]);
+//                    delete3DArray(arr,N,M);
+//                    return 0;
+//                }
+//                i++;
+//            }
+//        }
+//    }
+//    delete3DArray(arr,N,M);
+//    return 0;
 }
