d84aa0fc4069c5b50a138cb0c487f5d333812589
VAD <vladimir_ad@mail.ru>
2021-06-15 22:29:54 +0300

    Изменения, которые будут включены в коммит: 	новый файл:    STAVDETI3/Z_G/Z_G.cbp 	новый файл:    STAVDETI3/Z_G/main.cpp

---
+++ STAVDETI3/Z_G/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+
+
+char** newArray2D(int y, int x)
+{
+    char **p = new char*[y];
+    for(int j=0; j<y; j++)
+        p[j] = new char[x];
+    return p;
+}
+
+void deleteArray2D(char** p, int y)
+{
+    for(int j=0; j<y; j++)
+        delete[] p[j];
+    delete[] p;
+}
+
+void printArray(char **p, int maxY, int maxX)
+ {
+    for(int y = 0; y < maxY; y++)
+    {
+        for(int x = 0; x < maxX; x++)
+            cout << p[y][x] << " ";
+
+        cout << endl;
+    }
+}
+
+bool checkLayer(char *p)
+{
+    while(*p != 0)
+    {
+        if(*p != '*')
+            return false;
+        p++;
+    }
+    return true;
+}
+
+int main()
+{
+    int maxY, maxX;
+    cin >> maxY >> maxX;
+    cin.ignore();
+
+    char **buff = newArray2D(maxY, maxX +1);
+
+    for(int y = 0; y < maxY; y++)
+    {
+        char str[101];
+        cin.getline(str, 101);
+        for(int x = 0; x  < maxX; x++)
+        {
+            //buff[y][x] = inp[y][x];
+            buff[y][x] = str[x];
+        }
+        buff[y][maxX] = 0;  //для получения нультерминированной строки
+    }
+    //printArray(buff, maxY, maxX);
+
+    int n = 0;
+    int cnt = 0;
+    for(int y = 0; y < maxY; y++)
+    {
+        if(checkLayer(buff[y]) == false)
+        {
+            cnt += n*(n+1)/2;
+            n = 0;
+        }
+        else
+            n++;
+    }
+    if(n > 0)
+        cnt += n*(n+1)/2;
+
+    cout << cnt << endl;
+
+    deleteArray2D(buff, maxY);
+    return 0;
+}
