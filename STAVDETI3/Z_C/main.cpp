d5cd449bfcdf49195f4e0b09bbe4c2e4513a8004
DmitrievVA <DmitrievVA@monocrystal.com>
2021-06-15 08:45:14 +0300

    Changes to be committed: 	modified:   STAVDETI3/Z_C/main.cpp   //Предложено новое решение 	new file:   STAVDETI3/Z_C/main_.cpp

--- STAVDETI3/Z_C/main.cpp
+++ STAVDETI3/Z_C/main.cpp
@@ -2,34 +2,56 @@
 
 using namespace std;
 
-bool weight(int a, int b, int c, int w)
+typedef struct { int a; int b;} tent_t;
+
+bool test1(tent_t *p, int k, int w, int i0, int i1, int i2)
 {
-    if(a+b <= w || a+c <= w || b+c <= w || a+b+c <= w)
+    int a = 0;
+    int b = 0;
+    if(i0 >= 0)
+    {
+        a = p[i0].a;
+        b = p[i0].b;
+    }
+    if(i1 >= 0)
+    {
+        a += p[i1].a;
+        b += p[i1].b;
+    }
+    if(i2 >= 0)
+    {
+        a += p[i2].a;
+        b += p[i2].b;
+    }
+    if(b >= k  && a <= w)
         return true;
-    else
-        return false;
+
+    return false;
 }
 
-bool quantity(int a1, int b1, int c1, int k)
-{
-    if(a1+b1 <= k || a1+c1 <= k || b1+c1 <= k || a1+b1+c1 <= k)
-        return true;
-    else
-        return false;
-}
 int main()
 {
-    int k, w;
-    int a, b, c, a1, b1, c1;
+    //tent_t tent[3]  = {{5,5},{6,6},{7,7}};
+    tent_t tent[3];
+    int k; // = 10;
+    int w; // = 10;
     cin >> k >> w;
-    cin >> a >> a1 >> b >> b1 >> c >> c1;
+    for(int j = 0; j < 3; j++)
+        cin >> tent[j].a >> tent[j].b;
 
-    bool wres = weight(a, b, c, w);
-    bool kres = quantity(a1, b1, c1, k);
+    while(1)
+    {
+        if(test1(tent, k, w,  0, -1, -1)) break;
+        if(test1(tent, k, w,  0,  1, -1)) break;
+        if(test1(tent, k, w,  0, -1,  2)) break;
+        if(test1(tent, k, w,  0,  1,  2)) break;
+        if(test1(tent, k, w, -1,  1, -1)) break;
+        if(test1(tent, k, w, -1,  1,  2)) break;
+        if(test1(tent, k, w, -1, -1,  2)) break;
 
-    if(wres && kres)
-        cout << "YES"<< endl;
-    else
         cout << "NO" << endl;
+        return 0;
+    }
+    cout << "YES" << endl;
     return 0;
 }
