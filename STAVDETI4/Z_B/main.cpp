8090260688b746bf53a4af2284d535920273d147
Lemon <raccoonreckless@gmail.com>
2021-06-20 23:25:46 +0300

    NEW

--- STAVDETI4/Z_B/main.cpp
+++ STAVDETI4/Z_B/main.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <algorithm>
 
-void moveLeft(int* p, int n)
-{
-}
-
 using namespace std;
 
+int compare(const void* a, const void* b)
+{
+	int x = *static_cast<const int*>(a);
+	int y = *static_cast<const int*>(b);
+
+    int xa = x % 10;
+    int xb = y % 10;
+
+	if(xa > xb)
+		return 1;
+    if(xa < xb)
+        return -1;
+    if(x > y)
+    {
+        return 1;
+    }
+	return 0;
+}
 int main()
 {
-    int buf[101];
     int n;
     cin >> n;
+    int buf[101];
     for(int i = 0; i < n; i++)
         cin >> buf[i];
 
-    int buff[101];
-    for(int i = 0 ; i < n; i++)
-    {
-        int t = buf[n-1];
-        if(buf[i] > t)
-            buff[i] = buf[i];
-        else
-            buff[i] = 32001;
-    }
-    sort(buff, buff+n);
-    for(int i = 0; i < n; i++)
-    {
-        int t = buf[n-1];
-        if(buf[i] == t)
-            if()
-    }
+    qsort(buf, n, sizeof(int), compare);
+
     for(int i = 0; i < n; i++)
         cout << buf[i] << " ";
     return 0;
