468ba5799dadcec1f73dad785be418b10c9af223
Lemon <raccoonreckless@gmail.com>
2021-06-21 00:14:47 +0300

    C

--- STAVDETI4/Z_C/main.cpp
+++ STAVDETI4/Z_C/main.cpp
@@ -4,13 +4,39 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-    cin >> a >> b >> c;
-    int i = max(max(a,b), max(b,c));
-    while(i % a != 0 && i % b && i % c != 0)
+    int n;
+    cin >> n;
+    int sums = 0;
+    int sumt = 0;
+    int maxi1 = -1, maxi2 = 100001;
+    int res;
+
+    int buf[101][101];
+    for(int i = 0; i < n; i++)
     {
-        i++;
+        for(int j = 0; j < n; j++)
+        {
+            cin >> buf[i][j];
+            sums += buf[i][j];
+        }
+        if(sums > maxi1)
+            maxi1 = sums;
+
+        sums = 0;
     }
-    cout << i << endl;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            sumt += buf[j][i];
+        }
+        if(sumt < maxi2)
+            maxi2 = sumt;
+
+        sumt = 0;
+    }
+    res = buf[maxi1][maxi2];
+    cout << res;
     return 0;
 }
