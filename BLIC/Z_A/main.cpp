0f8c0f1c73618370719112029b929fc2cf6917eb
Lemon <raccoonreckless@gmail.com>
2021-09-14 17:28:00 +0300

    gyt

--- BLIC/Z_A/main.cpp
+++ BLIC/Z_A/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n, k;
+    cin >> n >> k;
+    int sum = 0;
+    k *= 2;
+    int ke1 = 1, ke2 = 1;
+    for(int i = 0; i < k; i += 2)
+    {
+        int x, y;
+        cin >> x >> y;
+        sum += abs(x - ke1) + abs(y - ke2);
+        ke1 = x;
+        ke2 = y;
+    }
+    cout << sum << endl;
     return 0;
 }
