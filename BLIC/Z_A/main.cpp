ab454b86685cd2c8fb9c37bbbaaf797be892c27c
Lemon <raccoonreckless@gmail.com>
2021-09-14 17:53:13 +0300

    h

--- BLIC/Z_A/main.cpp
+++ BLIC/Z_A/main.cpp
@@ -5,18 +5,30 @@ using namespace std;
 
 int main()
 {
-    int n, k;
-    cin >> n >> k;
+    int strike = 0;
+    int spare = 0;
     int sum = 0;
-    k *= 2;
-    int ke1 = 1, ke2 = 1;
-    for(int i = 0; i < k; i += 2)
+    for(int i = 0; i < 10; i++)
     {
-        int x, y;
-        cin >> x >> y;
-        sum += abs(x - ke1) + abs(y - ke2);
-        ke1 = x;
-        ke2 = y;
+        int b1, b2;
+        cin >> b1 >> b2;
+        if(strike > 1)
+            sum += (b1 + b2)*3;
+        else if(strike == 1)
+            sum += (b1 + b2)*2;
+        else if(spare > 0)
+            sum += b1*2 + b2;
+        else if(strike == 0 && spare == 0)
+            sum += b1 + b2;
+
+        if(b1 == 10)
+            strike++;
+        else
+            strike = 0;
+        if(b1 != 10 && b1 + b2 == 10)
+            spare++;
+        else
+            spare = 0;
     }
     cout << sum << endl;
     return 0;
