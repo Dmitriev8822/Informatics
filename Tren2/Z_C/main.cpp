281a8c58fc592419245bb6c77e3e7e5d1bff3749
Lemon <raccoonreckless@gmail.com>
2021-09-19 22:07:09 +0300

    WCh

--- Tren2/Z_C/main.cpp
+++ Tren2/Z_C/main.cpp
@@ -9,16 +9,16 @@ int main()
     if(n%100 == 0)
     {
         if(n%400 == 0)
-            cout << "13/09/";
-        else
             cout << "12/09/";
+        else
+            cout << "13/09/";
     }
     else
     {
         if(n % 4 == 0)
-            cout << "13/09/";
-        else
             cout << "12/09/";
+        else
+            cout << "13/09/";
     }
     cout.width(4);
     cout.fill('0');
