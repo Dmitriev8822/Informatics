217965e44c4772c9a2d307b06c0eb4f6d5888454
VAD <vladimir_ad@mail.ru>
2021-09-19 21:04:57 +0300

    изменено:      Tren2/Z_C/main.cpp //измененно условие выбора високосного года.

--- Tren2/Z_C/main.cpp
+++ Tren2/Z_C/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int n;
     cin >> n;
-    if((n % 4 == 0 && n % 100 != 0) || n % 400 == 0)
+    if(n % 4 == 0 || n % 400 == 0)
         cout << "12/09/" << n << endl;
     else
         cout << "13/09/" << n << endl;
