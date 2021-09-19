62b8815949d7099fc98ba5757db7e5dbebfc2b6f
VAD <vladimir_ad@mail.ru>
2021-09-19 21:17:18 +0300

    изменено:      Tren2/Z_C/main.cpp

--- Tren2/Z_C/main.cpp
+++ Tren2/Z_C/main.cpp
@@ -6,9 +6,18 @@ int main()
 {
     int n;
     cin >> n;
-    if(n % 4 == 0 || n % 400 == 0)
+    if(n % 400)
+    {
         cout << "12/09/" << n << endl;
-    else
-        cout << "13/09/" << n << endl;
+        return 0;
+    }
+    if(n % 4 == 0 && n % 100 != 0)
+    {
+        cout << "12/09/" << n << endl;
+        return 0;
+    }
+
+    cout << "13/09/" << n << endl;
+
     return 0;
 }
