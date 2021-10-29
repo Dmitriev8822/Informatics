ed5d5455f81d5d79068de83ce65868a47c1a8291
VAD <vladimir_ad@mail.ru>
2021-10-29 21:17:16 +0300

    новый файл:    BlicFFB/Z_B/Z_B.cbp новый файл:    BlicFFB/Z_B/main.cpp

---
+++ BlicFFB/Z_B/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int M[3];
+
+    cin >> M[0] >> M[1] >> M[2];
+    int I[] = {0,1,2,1};
+    int cnt = 0;
+    int index = 0;
+    while(1)
+    {
+        int v = M[I[index]];
+        if(v == 0)
+            break;
+        cnt++;
+        M[I[index]] = v-1;
+        index++;
+        if(index > 3)
+            index = 0;
+    }
+    cout << cnt;
+    return 0;
+}
